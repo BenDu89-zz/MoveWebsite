a87112d0496bb8b2ecf7bb3569d504938cf87398
Benjamin <bdunisch@googlemail.com>
2017-04-29 15:43:08 +0200

    Update Filmlist.py

--- Filmlist.py
+++ Filmlist.py
@@ -1,7 +1,9 @@
 import media
+''' media is holding the class that we a going to use, to creat diffrent Film objects '''
 import fresh_tomatoes
+''' fresh_tomatoes will take all your objects and turn them into a good looking website '''
 
-
+# below the diffrent objects that
 DieVerurteilten = media.Movies("Die Verurteilten", "Unschuldig im Knast",
                                 "https://www.youtube.com/watch?v=Uxgv5udgjjY",
                                 "https://images-na.ssl-images-amazon.com/images/I/517SDGYY26L.jpg")
@@ -15,8 +17,10 @@ StarWars = media.Movies("Das Imperium schlaegt zurueck", "Krieg",
                                 "http://vignette3.wikia.nocookie.net/jedipedia/images/d/d4/Episode_5_%28Roman%29.jpg/revision/latest?cb=20080801211647&path-prefix=de")
 
 movielist = [DieVerurteilten, HerrDerRinge, StarWars]
+# All objects in one list
 fresh_tomatoes.open_movies_page(movielist)
+# creating the movie page with the list of objects as an input
 
 print (media.Movies.VALID_RATINGS)
-doc = media.Movies.__module__
+doc = media.Movies.__module__ # look at the provided doc from media
 print doc
